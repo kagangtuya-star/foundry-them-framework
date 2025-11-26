8b013654a1689ea2f66d0b02e08feb13751ea618
SilverDragon <kagangtuya@foxmail.com>
2025-11-26 13:19:55 +0800

    修复：打包错误，绝对引入

--- installer_app/main.py
+++ installer_app/main.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 import sys
 from PyQt6 import QtWidgets
 
-from .gui import ThemeInstallerWindow
+try:
+    from .gui import ThemeInstallerWindow
+except ImportError:  # 打包为独立可执行时使用绝对导入
+    from installer_app.gui import ThemeInstallerWindow
 
 
 def main():
